a4127da8321bad599f3b80e2a6cc948c5e4da771
triszt4n <trisztanpiller@gmail.com>
2021-11-01 18:33:15 +0100

    Rework movement

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,26 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    public float speed = 10;
-
-    public float lifeTime = 5; //life of bullet in sec
-
+    private float lifeTime = 5f; // life of bullet in sec
+    private float speed = 20f;
+    
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(nameof(DieAfterExpired));
     }
-
+    
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * Time.fixedDeltaTime);
     }
 
-    IEnumerator DieAfterExpired()
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // todo: implement when there are collidables 
+    }
+
+    private IEnumerator DieAfterExpired()
     {
         yield return new WaitForSeconds(lifeTime);
         Destroy(gameObject);
