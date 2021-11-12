e09fdc629ec6d013006228668511fef01bf22094
Trisztán Piller <60111772+triszt4n@users.noreply.github.com>
2021-11-12 11:41:51 +0100

    Mini refactors (#22)
    * Shoot on mouse hold down
    
    * Bigger fov

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,9 @@ public class PlayerController : MonoBehaviour
     private Vector2 mousePos;
     
     public float moveSpeed = 10f;
+    public int framesBetweenShots = 30;
+    
+    private bool shootStarted = false;
 
     // Update is called once per frame
     void Update()
@@ -29,9 +32,17 @@ public class PlayerController : MonoBehaviour
         movement.y = Input.GetAxis("Vertical");
 
         mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        
-        if (Input.GetKeyDown(KeyCode.Mouse0)) 
-            Shoot();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            shootStarted = true;
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            shootStarted = false;
+            tempFrameCount = 0; // reset the counter
+        }
+
+        ShootIfStarted();
     }
 
     // src: https://www.youtube.com/watch?v=LNLVOjbrQj4&t=207s
@@ -43,6 +54,21 @@ public class PlayerController : MonoBehaviour
         Vector2 lookDir = mousePos - playerRb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         playerRb.rotation = angle;
+    }
+
+    private int tempFrameCount = 0;
+    
+    void ShootIfStarted()
+    {
+        if (!shootStarted)
+            return;
+        
+        if (tempFrameCount == 0)
+            Shoot();
+        
+        ++tempFrameCount;
+        if (tempFrameCount == framesBetweenShots)
+            tempFrameCount = 0;
     }
     
     void Shoot()
