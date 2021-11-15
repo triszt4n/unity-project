0a0bfb2e864ce10df825bf33980c3be29e9dd4b8
Beni <bucsy.beni@gmail.com>
2021-11-15 20:08:52 +0100

    Add SuperMode powerup

---
+++ Assets/Scripts/PowerUps/Payloads/SuperModePowerUpPayload.cs
@@ -0,0 +1,33 @@
+﻿namespace PowerUps
+{
+    public class SuperModePowerUpPayload: ITemporaryPowerUp
+    {
+
+        private float duration;
+        private float moveMultiplier;
+
+        public SuperModePowerUpPayload(float duration, float moveMultiplier)
+        {
+            this.duration = duration;
+            this.moveMultiplier = moveMultiplier;
+        }
+    
+
+        public void OnAttach(PlayerController player)
+        {
+            player.hasShield = true;
+            player.moveSpeed *= moveMultiplier;
+        }
+
+        public void OnDetach(PlayerController player)
+        {
+            player.hasShield = false;
+            player.moveSpeed /= moveMultiplier;
+        }
+
+        public float Duration()
+        {
+            return duration;
+        }
+    }
+}
