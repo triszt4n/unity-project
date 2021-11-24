e7320f7928ad7c9b31195daa3ce7bd07316c59ed
Beni <bucsy.beni@gmail.com>
2021-11-24 19:11:12 +0100

    Add man menu (#46)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
