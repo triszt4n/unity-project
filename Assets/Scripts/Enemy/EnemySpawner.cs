f53be70256ca5742e6d6b978b2d310fa46341b1c
vercibar <bercike445@gmail.com>
2021-11-24 17:39:58 +0100

    Enemies spawn in groups and don't spawn on screen #37 #41

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -16,16 +16,17 @@ namespace Enemy
         public GameObject player;
         public ScoreController scoreController;
         private DateTime nextEnemySpawnTime = DateTime.Now;
-        
+
         // Add Group spawning support
         private Dictionary<EnemyType, double> spawnChances = new Dictionary<EnemyType, double>()
         {
-            {EnemyType.Snitch, 0.5},
-            {EnemyType.Minion, 0.5},
-            {EnemyType.Walker, 0.5},
-            {EnemyType.Dodger, 0.5},
-            {EnemyType.Bumper, 0.5}
+            {EnemyType.Snitch, 0.3},
+            {EnemyType.Minion, 0.3},
+            {EnemyType.Walker, 0.3},
+            {EnemyType.Dodger, 0.3},
+            {EnemyType.Bumper, 0.3}
         };
+
         private void Start()
         {
             nextEnemySpawnTime = DateTime.Now + TimeSpan.FromSeconds(2.0f);
@@ -40,52 +41,80 @@ namespace Enemy
                 GameObject enemy = null;
                 foreach (EnemyType enemyType in enemyTypesToSpawn)
                 {
-                    switch (enemyType)
+                    for (int i = 0; i < AbstractEnemy.EnemyGroupSize(enemyType); i++)
                     {
-                        case EnemyType.Snitch:
+                        switch (enemyType)
                         {
-                            var snitch = Instantiate(snitchPrefab, GenerateEnemyStartingPosition(), Quaternion.Euler(0, 0, 0));
-                            enemy = snitch;
-                            snitch.GetComponent<SnitchController>().objectToRunFrom =
-                                player.GetComponent<Collider2D>();
-                            break;
+                            case EnemyType.Snitch:
+                            {
+                                var snitch = Instantiate(snitchPrefab, GenerateEnemyStartingPosition(),
+                                    Quaternion.Euler(0, 0, 0));
+                                enemy = snitch;
+                                snitch.GetComponent<SnitchController>().objectToRunFrom =
+                                    player.GetComponent<Collider2D>();
+                                break;
+                            }
+                            case EnemyType.Minion:
+                            {
+                                var minion = Instantiate(minionPrefab, GenerateEnemyStartingPosition(),
+                                    Quaternion.Euler(0, 0, 0));
+                                enemy = minion;
+                                minion.GetComponent<MinionController>().objectToChase =
+                                    player.GetComponent<Collider2D>();
+                                break;
+                            }
+                            case EnemyType.Walker:
+                            {
+                                enemy = Instantiate(walkerPrefab, GenerateEnemyStartingPosition(),
+                                    Quaternion.Euler(0, 0, 0));
+                                break;
+                            }
+                            case EnemyType.Dodger:
+                            {
+                                var dodger = Instantiate(dodgerPrefab, GenerateEnemyStartingPosition(),
+                                    Quaternion.Euler(0, 0, 0));
+                                enemy = dodger;
+                                dodger.GetComponent<DodgerController>().objectToChase =
+                                    player.GetComponent<Collider2D>();
+                                break;
+                            }
+                            case EnemyType.Bumper:
+                            {
+                                enemy = Instantiate(bumperPrefab, GenerateEnemyStartingPosition(),
+                                    Quaternion.Euler(0, 0, 0));
+                                break;
+                            }
                         }
-                        case EnemyType.Minion:
-                        {
-                            var minion = Instantiate(minionPrefab, GenerateEnemyStartingPosition(), Quaternion.Euler(0, 0, 0));
-                            enemy = minion;
-                            minion.GetComponent<MinionController>().objectToChase =
-                                player.GetComponent<Collider2D>();
-                            break;
-                        }
-                        case EnemyType.Walker:
-                        {
-                            enemy = Instantiate(walkerPrefab, GenerateEnemyStartingPosition(), Quaternion.Euler(0, 0, 0));
-                            break;
-                        }
-                        case EnemyType.Dodger:
-                        {
-                            var dodger = Instantiate(dodgerPrefab, GenerateEnemyStartingPosition(), Quaternion.Euler(0, 0, 0));
-                            enemy = dodger;
-                            dodger.GetComponent<DodgerController>().objectToChase =
-                                player.GetComponent<Collider2D>();
-                            break;
-                        }
-                        case EnemyType.Bumper:
-                        {
-                            enemy = Instantiate(bumperPrefab, GenerateEnemyStartingPosition(), Quaternion.Euler(0, 0, 0));
-                            break;
-                        }
+
+                        if (enemy != null) enemy.GetComponent<AbstractEnemy>().scoreController = scoreController;
                     }
-                    if (enemy != null) enemy.GetComponent<AbstractEnemy>().scoreController = scoreController;
                 }
             }
         }
 
-        // place enemies in other places
-        private Vector3 GenerateEnemyStartingPosition()
+        private bool CameraContainsPoint(Camera c, Vector2 v)
         {
-            return Random.insideUnitSphere * 10;
+            var viewPointPoint = c.WorldToViewportPoint(v);
+            // 0.1 margin, so we don't see the corners of the spawned enemies either
+            return -0.1 <= viewPointPoint.x &&
+                   viewPointPoint.x <= 1.1 &&
+                   -0.1 <= viewPointPoint.y &&
+                   viewPointPoint.y <= 1.1;
+        }
+        
+        // place enemies in other places
+        private Vector2 GenerateEnemyStartingPosition()
+        {
+            var camera = Camera.main;
+            // Removed 1 from the border, so they don't accidentally get pushed outside the border
+            // when spawning
+            Vector2 startPosition = new Vector2(Random.Range(-59, 59), Random.Range(-29, 29));
+            while (CameraContainsPoint(camera, startPosition))
+            {
+                startPosition = new Vector2(Random.Range(-59, 59), Random.Range(-29, 29));
+            }
+            Debug.Log("Generating start position:" + startPosition);
+            return startPosition;
         }
 
         private List<EnemyType> GenerateNextEnemyTypes()
