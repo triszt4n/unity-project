95b57e07e19d979ff94ce090ec8056099d3ca565
Trisztán Piller <60111772+triszt4n@users.noreply.github.com>
2021-11-26 03:48:13 +0100

    Revert "#36 difficulty levels (#47)"
    This reverts commit 05d6ed3f9e088cc6e7f330a39baa15f139f5a817.

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -16,9 +16,7 @@ namespace Enemy
         public GameObject player;
         public ScoreController scoreController;
         private DateTime nextEnemySpawnTime = DateTime.Now;
-        public DifficultyController difficultyController;
-        private DifficultyController.Difficulty difficulty;
-        
+
         // Add Group spawning support
         private Dictionary<EnemyType, double> spawnChances = new Dictionary<EnemyType, double>()
         {
@@ -32,75 +30,9 @@ namespace Enemy
         private void Start()
         {
             nextEnemySpawnTime = DateTime.Now + TimeSpan.FromSeconds(2.0f);
-            difficulty = difficultyController.GetDifficulty();
-            UpdateSpawnChancesForDifficulty();
         }
 
         private void Update()
-        {
-            UpdateSpawnChancesForDifficulty();
-        }
-
-        private void UpdateSpawnChancesForDifficulty()
-        {
-            // We don't touch the spawn chances if the difficulty hasn't changed
-            if (difficulty == difficultyController.GetDifficulty())
-            {
-                return;
-            }
-            difficulty = difficultyController.GetDifficulty();
-
-            switch (difficultyController.GetDifficulty())
-            {
-                case DifficultyController.Difficulty.Easy:
-                {
-                    spawnChances[EnemyType.Snitch] = 0.8;
-                    spawnChances[EnemyType.Minion] = 0.6;
-                    spawnChances[EnemyType.Walker] = 0.2;
-                    spawnChances[EnemyType.Dodger] = 0.01;
-                    spawnChances[EnemyType.Bumper] = 0.8;
-                    break;
-                }
-                case DifficultyController.Difficulty.Normal:
-                {
-                    spawnChances[EnemyType.Snitch] = 0.4;
-                    spawnChances[EnemyType.Minion] = 0.4;
-                    spawnChances[EnemyType.Walker] = 0.4;
-                    spawnChances[EnemyType.Dodger] = 0.05;
-                    spawnChances[EnemyType.Bumper] = 0.5;
-                    break;
-                }
-                case DifficultyController.Difficulty.Hard:
-                {
-                    spawnChances[EnemyType.Snitch] = 0.2;
-                    spawnChances[EnemyType.Minion] = 0.4;
-                    spawnChances[EnemyType.Walker] = 0.4;
-                    spawnChances[EnemyType.Dodger] = 0.2;
-                    spawnChances[EnemyType.Bumper] = 0.2;
-                    break;
-                }
-                case DifficultyController.Difficulty.VeryHard:
-                {
-                    spawnChances[EnemyType.Snitch] = 0.1;
-                    spawnChances[EnemyType.Minion] = 0.8;
-                    spawnChances[EnemyType.Walker] = 0.6;
-                    spawnChances[EnemyType.Dodger] = 0.5;
-                    spawnChances[EnemyType.Bumper] = 0.4;
-                    break;
-                }
-                case DifficultyController.Difficulty.Impossible:
-                {
-                    spawnChances[EnemyType.Snitch] = 0.01;
-                    spawnChances[EnemyType.Minion] = 1;
-                    spawnChances[EnemyType.Walker] = 1;
-                    spawnChances[EnemyType.Dodger] = 1;
-                    spawnChances[EnemyType.Bumper] = 1;
-                    break;
-                }
-            }
-        }
-
-        private void FixedUpdate()
         {
             if (DateTime.Now >= nextEnemySpawnTime)
             {
@@ -181,6 +113,7 @@ namespace Enemy
             {
                 startPosition = new Vector2(Random.Range(-59, 59), Random.Range(-29, 29));
             }
+            Debug.Log("Generating start position:" + startPosition);
             return startPosition;
         }
 
