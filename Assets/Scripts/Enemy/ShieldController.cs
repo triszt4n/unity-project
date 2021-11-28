33ff2f6f41a8b23bba93db9def6d4ff05cd72ba1
Beni <bucsy.beni@gmail.com>
2021-11-28 11:41:17 +0100

    Add shield enemy (#59)

--- Assets/Scripts/Enemy/ShieldController.cs
+++ Assets/Scripts/Enemy/ShieldController.cs
@@ -1,14 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Enemy
 {
     [RequireComponent(typeof(Collider2D))]
     public class ShieldController : MonoBehaviour
     {
-        private void OnTriggerEnter2D(Collider2D other)
+ 
+
+    private void OnTriggerEnter2D(Collider2D other)
         {
-            if(!other.gameObject.CompareTag("Projectile")) return;
-            Destroy(other.gameObject);
+            if (other.gameObject.CompareTag("Projectile"))
+            {
+                Destroy(other.gameObject);
+            }
         }
     }
 }
