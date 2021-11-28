a3d14cb82753afd3e534c6d91da43438ee66617b
Beni <bucsy.beni@gmail.com>
2021-11-28 11:54:21 +0100

    remove unnecesary prefabs

--- Assets/Scripts/PowerUps/PowerUpSpawner.cs
+++ Assets/Scripts/PowerUps/PowerUpSpawner.cs
@@ -10,14 +10,13 @@ namespace PowerUps
 
         public GameObject healthPrefab;
         public GameObject hologramPickupPrefab;
-        public GameObject hologramShipPrefab;
         public GameObject shieldPrefab;
         public GameObject gatePrefab;
         public GameObject superModePrefab;
-        public GameObject explosionPrefab;
         public float spawnTimeSpanStart = 2.0f;
         public float spawnTimeSpanEnd = 5.0f;
         private DateTime nextSpawnTime = DateTime.Now;
+        private Camera camera;
 
         // Add Group spawning support
         private Dictionary<PowerUpType, double> spawnChances = new Dictionary<PowerUpType, double>()
@@ -32,6 +31,7 @@ namespace PowerUps
         private void Start()
         {
             nextSpawnTime = DateTime.Now + TimeSpan.FromSeconds(Random.Range(spawnTimeSpanStart, spawnTimeSpanEnd));
+            camera = Camera.main;
         }
 
         private void Update()
@@ -42,21 +42,18 @@ namespace PowerUps
                 nextSpawnTime =
                     DateTime.Now + TimeSpan.FromSeconds(Random.Range(spawnTimeSpanStart, spawnTimeSpanEnd));
                 var toGenerate = GenerateNextPowerUpTypes();
-                GameObject powerUp = null;
                 foreach (PowerUpType powerUpType in toGenerate)
                 {
                     switch (powerUpType)
                     {
                         case PowerUpType.Gate:
-                            powerUp = Instantiate(gatePrefab, GeneratePowerUpStartingPosition(), Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
-                            powerUp.GetComponent<GateController>().explosionPrefab = explosionPrefab;
+                            Instantiate(gatePrefab, GeneratePowerUpStartingPosition(), Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
                             break;
                         case PowerUpType.Health:
                             Instantiate(healthPrefab, GeneratePowerUpStartingPosition(), Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
                             break;
                         case PowerUpType.Hologram:
-                            powerUp = Instantiate(hologramPickupPrefab, GeneratePowerUpStartingPosition(), Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
-                            powerUp.GetComponent<HologramPowerup>().hologramPrefab = hologramShipPrefab;
+                            Instantiate(hologramPickupPrefab, GeneratePowerUpStartingPosition(), Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
                             break;
                         case PowerUpType.Shield:
                             Instantiate(shieldPrefab, GeneratePowerUpStartingPosition(), Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
@@ -80,7 +77,6 @@ namespace PowerUps
         
         private Vector2 GeneratePowerUpStartingPosition()
         {
-            var camera = Camera.main;
             Vector2 startPosition = new Vector2(Random.Range(-55, 55), Random.Range(-25, 25));
             while (CameraContainsPoint(camera, startPosition))
             {
