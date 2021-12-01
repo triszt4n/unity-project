fcb83acf19c6eeea48e8593c3e7a02999228a3ff
Beni <bucsy.beni@gmail.com>
2021-12-01 19:20:52 +0100

    add shield effect

--- Assets/Scripts/PowerUps/Payloads/ShieldPowerUpPayload.cs
+++ Assets/Scripts/PowerUps/Payloads/ShieldPowerUpPayload.cs
@@ -1,19 +1,29 @@
-﻿namespace PowerUps
+﻿using UnityEngine;
+
+namespace PowerUps
 {
     public class ShieldPowerUpPayload : TemporaryPowerUp
     {
-        public ShieldPowerUpPayload(float duration) : base(duration)
+        private GameObject shieldPrefab;
+        private GameObject shieldObject;
+
+        public ShieldPowerUpPayload(float duration, GameObject shieldPrefab) : base(duration)
         {
+            this.shieldPrefab = shieldPrefab;
         }
 
         protected override void onAttach(PlayerController player)
         {
+            shieldObject = GameObject.Instantiate(shieldPrefab, player.gameObject.transform.position,
+                player.gameObject.transform.rotation);
+            shieldObject.transform.parent = player.gameObject.transform;
             player.hasShield = true;
         }
 
         protected override void onDetach(PlayerController player)
         {
             player.hasShield = false;
+            GameObject.Destroy(shieldObject);
         }
     }
 }
